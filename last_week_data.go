18604402ec903854edd8496d00fdad7499b71ee1
Ryan Hollis <ryanh@squareup.com>
2022-02-11 18:13:26 -0500

    Split up huge main.go file

---
+++ last_week_data.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/slack-go/slack"
+)
+
+func dealWithLastWeekMessages(allEmojis *SlackEmojiResponseMessage) error {
+	// Get the emojis channel
+	emojiChannelData, err := getChannel(emojiChannel)
+	if err != nil {
+		return err
+	}
+
+	// Get the two messages that we need
+	reactionMessage, lastEmojiMessage, err := findLastWeekMessages(emojiChannelData)
+	if err != nil {
+		return err
+	}
+	err = printTopEmojisByReactionVote(reactionMessage, allEmojis)
+	if err != nil {
+		return err
+	}
+	// Find the last emoji that was posted last week.
+	parts := strings.Split(lastEmojiMessage.Text, ":")
+	if len(parts) < 2 {
+		return fmt.Errorf("Unable to get last emoji from message %v", lastEmojiMessage)
+	}
+	lastNewEmoji = parts[len(parts)-2]
+	return nil
+}
+
+func findLastWeekMessages(emojiChannelData *slack.Channel) (*slack.Message, *slack.Message, error) {
+	conversationParams := &slack.GetConversationHistoryParameters{
+		ChannelID: emojiChannelData.ID,
+	}
+	var reactionMessage, lastEmojiMessage slack.Message
+	var foundOne, foundBoth bool
+	for true {
+		messages, err := slackApi.GetConversationHistory(conversationParams)
+		if err != nil {
+			return nil, nil, err
+		}
+		if foundOne {
+			if len(messages.Messages) == 0 {
+				return nil, nil, errors.New("Unable to find message " + emojiChannel)
+			}
+			lastEmojiMessage = messages.Messages[0]
+			foundBoth = true
+			break
+		}
+		for i, message := range messages.Messages {
+			if message.Text == lastMessage {
+				reactionMessage = message
+				foundOne = true
+				if len(messages.Messages) >= i {
+					lastEmojiMessage = messages.Messages[i+1]
+					foundBoth = true
+					break
+				}
+			}
+		}
+		if foundBoth {
+			break
+		}
+		if len(messages.ResponseMetadata.Cursor) == 0 {
+			return nil, nil, errors.New("Unable to find message in channel " + emojiChannel)
+		}
+		// Check if we have looked through 15 days
+		seconds, err := strconv.ParseFloat(messages.Messages[len(messages.Messages)-1].Timestamp, 64)
+		if err != nil {
+			return nil, nil, err
+		}
+		lastMessageTime := time.Unix(0, int64(float64(time.Second)*seconds))
+		if time.Since(lastMessageTime) > time.Hour*24*15 {
+			return nil, nil, errors.New("Unable to find message in channel " + emojiChannel + " in the last 15 days")
+		}
+		conversationParams.Cursor = messages.ResponseMetadata.Cursor
+	}
+	return &reactionMessage, &lastEmojiMessage, nil
+}
+
+func getChannel(channelName string) (*slack.Channel, error) {
+	if len(channelName) == 0 {
+		return nil, errors.New("No channel name provided")
+	}
+	if channelName[0] == '#' {
+		channelName = channelName[1:]
+	}
+	channelsParams := &slack.GetConversationsParameters{
+		ExcludeArchived: true,
+		Limit:           1000,
+	}
+	var emojiChannelData slack.Channel
+	for true {
+		channels, cursor, err := slackApi.GetConversations(channelsParams)
+		if err != nil {
+			return nil, err
+		}
+		var found bool
+		for _, channel := range channels {
+			if channel.IsChannel && channel.Name == channelName {
+				found = true
+				emojiChannelData = channel
+				break
+			}
+		}
+		if found {
+			break
+		}
+		if cursor == "" {
+			return nil, errors.New("Unable to find channel " + emojiChannel)
+		}
+		channelsParams.Cursor = cursor
+	}
+	return &emojiChannelData, nil
+}
+
+// This takes in the copied text of a message from Slack, and prints the top Emoji reactions.
+func printTopEmojisByReactionVote(message *slack.Message, allEmojis *SlackEmojiResponseMessage) error {
+	var emojis []*stringCount
+	for _, reaction := range message.Reactions {
+		emojis = append(emojis, &stringCount{name: reaction.Name, count: reaction.Count})
+	}
+	sort.Sort(ByCount(emojis))
+
+	printedCount := 0
+	previousCount := math.MaxInt64
+
+	maxPrintCount := 10
+	minReaction := 3
+	var creators []string
+	var counts []int
+	var printedEmojis []string
+	for _, emoji := range emojis {
+		// Stop if we have printed enough emojis, however, always print all emojis with the same reaction
+		// count even if we go over the limit.
+		if emoji.count != previousCount && printedCount >= maxPrintCount {
+			break
+		}
+		// Stop if the reaction count is too low, even if we have not hit the limit.
+		if emoji.count < minReaction {
+			break
+		}
+		emojisObj := allEmojis.emojiMap[emoji.name]
+		creators = append(creators, emojisObj.UserId)
+		counts = append(counts, emoji.count)
+		printedEmojis = append(printedEmojis, emoji.name)
+		previousCount = emoji.count
+		printedCount++
+	}
+
+	return printTopCreators(lastWeek, creators, counts, printedEmojis)
+}
---
+++ response_caching.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"sort"
+	"strings"
+	"time"
+)
+
+const (
+	snapshotDir = "/Documents/emojiSnapshots/"
+	imagesDir   = snapshotDir + "images/"
+)
+
+func ensureDirExists(path string) error {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return os.Mkdir(path, 0777)
+	}
+	return nil
+}
+
+func cacheEmojiResponse(commandResponse []byte) error {
+	userDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	err = ensureDirExists(userDir + snapshotDir)
+	if err != nil {
+		return err
+	}
+	fileName := userDir + snapshotDir + time.Now().String() + ".json"
+	return ioutil.WriteFile(fileName, commandResponse, 0644)
+}
+
+func cacheEmojiImages(response *SlackEmojiResponseMessage) error {
+	if cacheImages {
+		userDir, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		err = ensureDirExists(userDir + imagesDir)
+		if err != nil {
+			return err
+		}
+		// Download images for all emojis
+		for _, emoji := range response.Emoji {
+			if strings.HasPrefix(emoji.Url, "data:") {
+				// Handle base64 images
+				i := strings.Index(emoji.Url, ";")
+				ext := emoji.Url[len("data:image/"):i]
+				imagePath := userDir + imagesDir + emoji.Name + "." + ext
+				_, err := os.Stat(imagePath)
+				if os.IsNotExist(err) {
+					i = strings.Index(emoji.Url, ",")
+					dec := base64.NewDecoder(base64.StdEncoding, strings.NewReader(emoji.Url[i+1:]))
+					output, err := ioutil.ReadAll(dec)
+					if err != nil {
+						return err
+					}
+					err = ioutil.WriteFile(imagePath, output, 0644)
+					if err != nil {
+						return err
+					}
+				}
+			} else {
+				// Handle URL images
+				imagePath := userDir + imagesDir + emoji.Name + path.Ext(emoji.Url)
+				_, err := os.Stat(imagePath)
+				if os.IsNotExist(err) {
+					resp, err := http.Get(emoji.Url)
+					if err != nil {
+						return err
+					}
+					file, err := os.Create(imagePath)
+					if err != nil {
+						return err
+					}
+					_, err = io.Copy(file, resp.Body)
+					if err != nil {
+						return err
+					}
+					err = file.Close()
+					if err != nil {
+						return err
+					}
+				} else if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func detectDeletedEmojis(response *SlackEmojiResponseMessage) error {
+	var message string
+	lastResponseBytes, err := readLastEmojiDump(1)
+	if err != nil {
+		return err
+	}
+
+	if lastResponseBytes != nil {
+		allCurrentEmojis := make(map[string]struct{})
+		for _, emoji := range response.Emoji {
+			allCurrentEmojis[emoji.Name] = struct{}{}
+		}
+		lastResponse, err := parseEmojiResponse(lastResponseBytes)
+		if err != nil {
+			return err
+		}
+		message += "\nDeleted Emojis:\n\n"
+		var missingEmojis []emoji
+		var peopleIds []string
+		for _, emoji := range lastResponse.Emoji {
+			if _, ok := allCurrentEmojis[emoji.Name]; !ok {
+				missingEmojis = append(missingEmojis, emoji)
+				peopleIds = append(peopleIds, emoji.UserId)
+			}
+		}
+		if len(peopleIds) > 0 {
+			userMap, err := getUsers(peopleIds)
+			if err != nil {
+				return err
+			}
+			for _, emoji := range missingEmojis {
+				user := userMap[emoji.UserId]
+				message += fmt.Sprintf("%s (@%s) %v %s \n", emoji.Name, user.Name, time.Unix(int64(emoji.Created), 0), emoji.Url)
+			}
+		}
+		message += "\n"
+	}
+	_, err = printMessage(MSG_TYPE__PRINT_ONLY, message)
+	return err
+}
+
+func readLastEmojiDump(offset int) ([]byte, error) {
+	if offset < 0 {
+		return nil, fmt.Errorf("negative offset not allowed. Offset was %d", offset)
+	}
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	files, err := ioutil.ReadDir(dirname + snapshotDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	fileNames := make([]string, 0, len(files))
+	for _, file := range files {
+		// Skip directories and hidden files
+		if !file.IsDir() && file.Name()[0] != '.' {
+			fileNames = append(fileNames, file.Name())
+		}
+	}
+	if len(fileNames) <= offset {
+		return nil, nil
+	}
+	sort.Strings(fileNames)
+	selectedName := fileNames[len(fileNames)-1-offset]
+	fileContents, err := ioutil.ReadFile(dirname + snapshotDir + selectedName)
+	if err != nil {
+		return nil, err
+	}
+	return fileContents, nil
+}
